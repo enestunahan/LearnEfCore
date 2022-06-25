e11d0e02cc6688aa043daeb1c1eee7f635229713
Enes Tunahan <enes.comez@yaz.com.tr>
2022-06-26 01:20:47 +0300

    Db bağlantısı kuruldu  , ilk migration oluşturuldu

---
+++ LearnEfCore/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LearnEfCore.Models
+{
+    public class AppDbContext :  DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasKey(e => e.Id);
+            modelBuilder.Entity<Book>().Property(e => e.Title).IsRequired();    
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ LearnEfCore/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿//using System.ComponentModel.DataAnnotations;
+
+namespace LearnEfCore.Models
+{
+    public class Book
+    {
+        //[Key] Property Constraints için  bu şekilde data annotation kullanmak tercih edilmemesi gereken bir yaklaşım
+        // bunun yerine fluent api kullanacağız
+        public int Id { get; set; } 
+        public string Title { get; set; }   
+    }
+}
